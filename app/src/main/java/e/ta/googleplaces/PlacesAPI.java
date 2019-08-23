3ad6b4f3128f0ee3f7f9d04dcb0370b3234591b4
Tilbe Aksoy <51287067+tilbeaksoy@users.noreply.github.com>
2019-08-23 15:24:05 +0300

    Update PlacesAPI.java

--- app/src/main/java/e/ta/googleplaces/PlacesAPI.java
+++ app/src/main/java/e/ta/googleplaces/PlacesAPI.java
@@ -6,6 +6,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface PlacesAPI {
-    @GET("maps/api/place/nearbysearch/json?location=-33.8670,151.1957&radius=500&types=atm&key=AIzaSyCf_MjCA1D6TMNVe6_KofhR387FNZgZARc")
+    @GET("maps/api/place/nearbysearch/json?location=-33.8670,151.1957&radius=500&types=atm&key=API-KEY")
    Call<Places> getPlaces();
 }
